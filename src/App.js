39668c6684fbade9b83781c3d8f9a1f664f66c7b
Nate Swenson <nswenson@gl-macbook-191.local>
2023-01-17 15:20:29 -0800

    nate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
+import '@tremor/react/dist/esm/tremor.css';
+import Base from './components/Base'
+import Metrics from './components/Metrics'
 import './App.css';
+import json from './base.json';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Base json={json}/>
+      </div>
+    );
+  }
 }
 
 export default App;
